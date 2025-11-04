a7369940527a89b4b59f12567cc4f7df65f3ef49
apiando <apiando@bu.edu>
2025-11-04 17:00:42 -0500

    Revise rsvp.py to require user authentication for RSVP creation

--- spark-bytes/backend/routes/rsvp.py
+++ spark-bytes/backend/routes/rsvp.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import SessionLocal
 from models.rsvp import RSVP
-
+from routes.auth import get_current_user
 
 router = APIRouter(prefix="/rsvp", tags=["RSVP"])
 
@@ -14,15 +14,23 @@ def get_db():
         db.close()
 
 @router.post("/{event_id}")
-def create_rsvp(event_id: int, rsvp: dict, db: Session = Depends(get_db)):
-    new_rsvp = RSVP(
-        event_id=event_id,
-        name=rsvp.get("name"),
-        email=rsvp.get("email"),
-        food_item=rsvp.get("food_item"),
-        quantity=rsvp.get("quantity"),
-    )
-    db.add(new_rsvp)
-    db.commit()
-    db.refresh(new_rsvp)
-    return {"success": True, "message": "Reservation created!", "rsvp": new_rsvp}
+def create_rsvp(
+        event_id: int, 
+        rsvp: dict, 
+        db: Session = Depends(get_db),
+        user = Depends(get_current_user),
+    ):
+        new_rsvp = RSVP(
+            event_id=event_id,
+            name=rsvp.get("name"),
+            email=rsvp.get("email"),
+            food_item=rsvp.get("food_item"),
+            quantity=rsvp.get("quantity"),
+            user_id=user.id,
+        )
+
+        db.add(new_rsvp)
+        db.commit()
+        db.refresh(new_rsvp)
+
+        return {"success": True, "message": "Reservation created!", "rsvp": new_rsvp}
