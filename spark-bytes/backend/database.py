04b1a937539cd966eb2d51a51aa84117f3ca980c
apiando <apiando@bu.edu>
2025-11-04 16:35:41 -0500

    Created user.py, auth.py, and .env; Made small revisions to database.py

--- spark-bytes/backend/database.py
+++ spark-bytes/backend/database.py
@@ -1,11 +1,18 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
 
-# Example local DB connection string
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://localhost/sparkbytes")
+# ensure .env variables are read
+load_dotenv()  
 
-engine = create_engine(DATABASE_URL)
+# local DB connection string
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 meta = MetaData()
