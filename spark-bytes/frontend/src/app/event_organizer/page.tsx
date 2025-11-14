fa4bbf7ac3b43abf931cbd1cbb7e2cb2a7836874
karachio <karachio@bu.edu>
2025-11-14 09:29:18 -0500

    added event organizer page and other changes

---
+++ spark-bytes/frontend/src/app/event_organizer/page.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function EventOrganizer() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* Navbar */}
+      <nav className="bg-black text-white flex justify-between items-center px-8 py-4">
+        <Image src="/next.svg" alt="Spark!Bytes logo" width={100} height={100} />
+        <ul className="flex space-x-6 text-lg">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/events">Events</Link>
+          </li>
+          <li>
+            <Link href="/create-event">Create Event</Link>
+          </li>
+          <li>
+            <Link href="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Section with list of who sogned up and for what food + amount of food */}
+    
+
+      {/* Footer */}
+      <footer className="bg-black text-white text-center text-sm py-6 px-4 mt-auto">
+        <p>Boston University Center of Computing & Data Sciences: Duan Family Spark! Initiative</p>
+        <p>665 Commonwealth Ave., Boston, MA 02215 | Floor 2, Spark! Space</p>
+        <p>buspark@bu.edu</p>
+      </footer>
+    </div>
+  );
+}
