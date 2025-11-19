f59d21e3e32923b7d8c32b019c2d48750a06891c
apiando <apiando@bu.edu>
2025-11-19 00:12:00 -0500

    Finished writing tests in backend/tests/test_auth.py

--- spark-bytes/backend/main.py
+++ spark-bytes/backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 # import routes
-from routes import events, rsvp, auth  # <-- add auth here
+from routes import events, rsvp, auth
 
 # initialize app
 app = FastAPI(title="SparkBytes API", version="0.2.0")
